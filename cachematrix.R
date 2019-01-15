14f495ec3c3a7d51bd48d27743f28ea2b4de2213
Benjamin Jäger <bj@bjmac.dyndns.cern.ch>
2019-01-15 20:16:08 +0100

    Add functions for progAss2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,32 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        matrix <<- y
+        inv <<- NULL
+    }
+    get <- function() matrix
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(matrix, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- matrix$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- matrix$get()
+    inv <- solve(data, ...)
+    matrix$setinv(inv)
+    inv
 }
